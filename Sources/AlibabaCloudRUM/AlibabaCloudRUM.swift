c18c642d33eb9e6cf0373b4b1d1a9a6b64d89086
yulong.gyl <keyu404@gmail.com>
2024-08-08 10:19:03 +0800

    feat: add custom event & metric & log api

--- Sources/AlibabaCloudRUM/AlibabaCloudRUM.swift
+++ Sources/AlibabaCloudRUM/AlibabaCloudRUM.swift
@@ -17,12 +17,18 @@ import OpenRUM
 
 @objc
 public class AlibabaCloudRUM : NSObject {
+    
+    @objc
+    public enum Env: Int {
+        case NONE
+        case PROD
+        case GRAY
+        case PRE
+        case DAILY
+        case LOCAL
+    }
+    
     private static let shared: AlibabaCloudRUM = AlibabaCloudRUM()
-    //    private let rum: OpenRUM?
-    //
-    //    public override init() {
-    //        super.init()
-    //    }
     
     
     @objc(startWithAppID:)
@@ -33,13 +39,40 @@ public class AlibabaCloudRUM : NSObject {
     @objc
     public static func setConfigAddress(_ configAddress: String) {
         OpenRUM.setConfigAddress(configAddress)
+        OpenRUM.setAppEnvironment(ORAppEnvironment.prod)
     }
-
+    
+    @objc
+    public static func setEnvironment(_ env: Env) {
+        switch env {
+        case .NONE:
+            OpenRUM.setAppEnvironment(ORAppEnvironment.none)
+            break
+        case .PROD:
+            OpenRUM.setAppEnvironment(ORAppEnvironment.prod)
+            break
+        case .GRAY:
+            OpenRUM.setAppEnvironment(ORAppEnvironment.gray)
+            break
+        case .PRE:
+            OpenRUM.setAppEnvironment(ORAppEnvironment.pre)
+            break
+        case .DAILY:
+            OpenRUM.setAppEnvironment(ORAppEnvironment.daily)
+            break
+        case .LOCAL:
+            OpenRUM.setAppEnvironment(ORAppEnvironment.local)
+            break
+        default:
+            OpenRUM.setAppEnvironment(ORAppEnvironment.prod)
+        }
+    }
+    
     @objc
     public static func setAppVersion(_ appVersion: String) {
         OpenRUM.setAppVersion(appVersion)
     }
-
+    
     @objc
     public static func setChannelID(_ channelID: String) {
         OpenRUM.setChannelID(channelID)
@@ -65,5 +98,25 @@ public class AlibabaCloudRUM : NSObject {
     @objc
     public static func setLogFlag(_ flag: NSNumber) {
         OpenRUM.setLogFlag(flag)
+    }
+    
+    @objc
+    public static func setCustomException(_ exceptionType: String, _ causeBy: String?, _ errorDump: String?) {
+        OpenRUM.setCustomExceptionWithType(exceptionType, causeBy: causeBy, errorDump: errorDump)
+    }
+    
+    @objc
+    public static func setCustomMetric(_ metricName: String, _ value: Int, _ param: String?) {
+        OpenRUM.setCustomMetricWithName(metricName, value: value, param: param)
+    }
+    
+    @objc
+    public static func setCustomEvent(_ eventID: String, _ name: String?, _ label: String?, _ param: String?, _ info: [String: String]?) {
+        OpenRUM.setCustomEventWithID(eventID, name: name, label: label, param: param, info: info)
+    }
+    
+    @objc
+    public static func setCustomLog(_ logInfo: String, _ param: String?) {
+        OpenRUM.setCustomLog(logInfo, param: param)
     }
 }
