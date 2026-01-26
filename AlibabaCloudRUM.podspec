6bc374c1db091a9432968f0d45ad3e0607361c47
yulong.gyl <keyu404@gmail.com>
2026-01-26 17:58:32 +0800

    version: 2.2.0-beta.1

--- AlibabaCloudRUM.podspec
+++ AlibabaCloudRUM.podspec
@@ -4,7 +4,7 @@
 
 Pod::Spec.new do |s|
     s.name             = 'AlibabaCloudRUM'
-    s.version          = "2.1.0"
+    s.version          = "2.2.0-beta.1"
     s.summary          = 'AlibabaCloud RUM service for iOS.'
 
     s.description      = <<-DESC
--- Sources/AlibabaCloudRUM/AlibabaCloudRUM.swift
+++ Sources/AlibabaCloudRUM/AlibabaCloudRUM.swift
@@ -17,7 +17,7 @@ import AlibabaCloudRUMSDK
 
 @objc
 public class AlibabaCloudRUM : NSObject {
-    private static let RUM_SDK_VERSION = "2.1.0"
+    private static let RUM_SDK_VERSION = "2.2.0-beta.1"
     
     private static var env: String?
     private static var endpoint: String?
