4fd27a0b8a47c9a0ff17999a3db027c357050be1
yulong.gyl <yulong.gyl@alibaba-inc.com>
2026-04-17 14:37:45 +0800

    feat(ios): add NS_SWIFT_NAME annotations to public API methods
    Annotate all single- and multi-parameter class methods in
    AlibabaCloudRUM.h with explicit NS_SWIFT_NAME to produce
    idiomatic Swift call sites and stabilize the Swift interface
    independent of Obj-C selector naming.

--- Sources/AlibabaCloudRUM/include/AlibabaCloudRUM.h
+++ Sources/AlibabaCloudRUM/include/AlibabaCloudRUM.h
@@ -72,109 +72,107 @@ typedef NS_ENUM(NSInteger, AlibabaCloudTracingProtocol) {
 
 + (void)disableSwizzleMethod:(SEL)selector clazz:(Class)clazz;
 
-+ (void)disableInspectClass:(NSString *)className
++ (void)disableInspectClass:(NSString *)className NS_SWIFT_NAME(disableInspectClass(_:))
     __attribute__((deprecated("This method has been renamed. Please use excludeClassFromTracking(_:) instead.")));
 
-+ (void)excludeClassFromTracking:(NSString *)className;
++ (void)excludeClassFromTracking:(NSString *)className NS_SWIFT_NAME(excludeClassFromTracking(_:));
 
-+ (void)enableBattery:(BOOL)enable
-    __attribute__((deprecated));
++ (void)enableBattery:(BOOL)enable NS_SWIFT_NAME(enableBattery(_:)) __attribute__((deprecated));
 
-+ (void)disableModule:(AlibabaCloudModule)module;
-+ (void)enableModule:(AlibabaCloudModule)module;
++ (void)disableModule:(AlibabaCloudModule)modul NS_SWIFT_NAME(disableModule(_:));
++ (void)enableModule:(AlibabaCloudModule)modul NS_SWIFT_NAME(enableModule(_:));
 
-+ (void)enableTrackingWebView:(BOOL)enable
++ (void)enableTrackingWebView:(BOOL)enable NS_SWIFT_NAME(enableTrackingWebView(_:))
     __attribute__((deprecated("Use disableModule(.webView) / enableModule(.webView) instead.")));
 
-+ (void)start:(NSString *)serviceId;
++ (void)start:(NSString *)serviceId NS_SWIFT_NAME(start(_:));
 
 + (void)startWithAppID:(NSString *)appID
     __attribute__((deprecated("Use start: instead.")));
 
-+ (void)setEndpoint:(NSString *)endpoint;
++ (void)setEndpoint:(NSString *)endpoint NS_SWIFT_NAME(setEndpoint(_:));
 
-+ (void)setConfigAddress:(NSString *)configAddress
++ (void)setConfigAddress:(NSString *)configAddress NS_SWIFT_NAME(setConfigAddress(_:))
     __attribute__((deprecated("This method has been renamed. Please use setEndpoint(_:) instead.")));
 
-+ (void)setWorkspace:(NSString *)workspace;
-+ (void)setEnvironment:(AlibabaCloudEnv)env;
-+ (void)setCustomEnvironment:(NSString * _Nullable)env;
-+ (void)setAppVersion:(NSString *)appVersion;
-+ (void)setChannelID:(NSString *)channelID;
-+ (void)setDeviceID:(NSString *)deviceID;
++ (void)setWorkspace:(NSString *)workspace NS_SWIFT_NAME(setWorkspace(_:));
++ (void)setEnvironment:(AlibabaCloudEnv)env NS_SWIFT_NAME(setEnvironment(_:));
++ (void)setCustomEnvironment:(NSString * _Nullable)env NS_SWIFT_NAME(setCustomEnvironment(_:));
++ (void)setAppVersion:(NSString *)appVersion NS_SWIFT_NAME(setAppVersion(_:));
++ (void)setChannelID:(NSString *)channelID NS_SWIFT_NAME(setChannelID(_:));
++ (void)setDeviceID:(NSString *)deviceID NS_SWIFT_NAME(setDeviceID(_:));
 
 @property (nonatomic, class, readonly, copy) NSString *deviceID;
 
-+ (void)setUserName:(NSString *)userID;
-+ (void)setExtraInfo:(NSDictionary<NSString *, id> *)extraInfo;
-+ (void)addExtraInfo:(NSDictionary<NSString *, id> *)extraInfo;
-+ (void)setUserExtraInfo:(NSDictionary<NSString *, id> *)extraInfo;
-+ (void)addUserExtraInfo:(NSDictionary<NSString *, id> *)extraInfo;
-+ (void)setAppFramework:(AlibabaCloudFramework)framework;
++ (void)setUserName:(NSString *)userID NS_SWIFT_NAME(setUserName(_:));
++ (void)setExtraInfo:(NSDictionary<NSString *, id> *)extraInfo NS_SWIFT_NAME(setExtraInfo(_:));
++ (void)addExtraInfo:(NSDictionary<NSString *, id> *)extraInfo NS_SWIFT_NAME(addExtraInfo(_:));
++ (void)setUserExtraInfo:(NSDictionary<NSString *, id> *)extraInfo NS_SWIFT_NAME(setUserExtraInfo(_:));
++ (void)addUserExtraInfo:(NSDictionary<NSString *, id> *)extraInfo NS_SWIFT_NAME(addUserExtraInfo(_:));
++ (void)setAppFramework:(AlibabaCloudFramework)framework NS_SWIFT_NAME(setAppFramework(_:));
 
-+ (void)setLogFlag:(NSNumber *)flag
++ (void)setLogFlag:(NSNumber *)flag NS_SWIFT_NAME(setLogFlag(_:))
     __attribute__((deprecated("This method has been renamed. Please use setDebuggable(_:) instead.")));
 
-+ (void)setDebuggable:(BOOL)debuggable;
-+ (void)stopSDK;
++ (void)setDebuggable:(BOOL)debuggable NS_SWIFT_NAME(setDebuggable(_:));
++ (void)stopSDK NS_SWIFT_NAME(stopSDK());
 
 + (void)setCustomException:(NSString *)exceptionType
                    causeBy:(NSString *)causeBy
-                 errorDump:(NSString *)errorDump;
+                 errorDump:(NSString *)errorDump NS_SWIFT_NAME(setCustomException(_:causeBy:errorDump:));
 
 + (void)setCustomMetric:(NSString *)metricName
                   value:(NSInteger)value
-                  param:(NSString * _Nullable)snapshots
-    NS_SWIFT_NAME(setCustomMetric(_:value:snapshots:));
+                  param:(NSString * _Nullable)snapshots NS_SWIFT_NAME(setCustomMetric(_:value:snapshots:));
 
-+ (void)setCustomEvent:(NSString *)name;
++ (void)setCustomEvent:(NSString *)name NS_SWIFT_NAME(setCustomEvent(_:));
 + (void)setCustomEvent:(NSString *)name
-                 group:(NSString * _Nullable)group;
-+ (void)setCustomEvent:(NSString *)name
-                 group:(NSString * _Nullable)group
-             snapshots:(NSString * _Nullable)snapshots;
+                 group:(NSString * _Nullable)group NS_SWIFT_NAME(setCustomEvent(_:group:));
 + (void)setCustomEvent:(NSString *)name
                  group:(NSString * _Nullable)group
-                 value:(double)value;
+             snapshots:(NSString * _Nullable)snapshots NS_SWIFT_NAME(setCustomEvent(_:group:snapshots:));
 + (void)setCustomEvent:(NSString *)name
                  group:(NSString * _Nullable)group
-                  info:(NSDictionary<NSString *, NSString *> * _Nullable)info;
+                 value:(double)value NS_SWIFT_NAME(setCustomEvent(_:group:value:));
 + (void)setCustomEvent:(NSString *)name
                  group:(NSString * _Nullable)group
-             snapshots:(NSString * _Nullable)snapshots
-                 value:(double)value;
+                  info:(NSDictionary<NSString *, NSString *> * _Nullable)info NS_SWIFT_NAME(setCustomEvent(_:group:info:));
 + (void)setCustomEvent:(NSString *)name
                  group:(NSString * _Nullable)group
              snapshots:(NSString * _Nullable)snapshots
-                  info:(NSDictionary<NSString *, NSString *> * _Nullable)info;
+                 value:(double)value NS_SWIFT_NAME(setCustomEvent(_:group:snapshots:value:));
++ (void)setCustomEvent:(NSString *)name
+                 group:(NSString * _Nullable)group
+             snapshots:(NSString * _Nullable)snapshots
+                  info:(NSDictionary<NSString *, NSString *> * _Nullable)info NS_SWIFT_NAME(setCustomEvent(_:group:snapshots:info:));
 + (void)setCustomEvent:(NSString *)name
                  group:(NSString * _Nullable)group
                  value:(double)value
-                  info:(NSDictionary<NSString *, NSString *> * _Nullable)info;
+                  info:(NSDictionary<NSString *, NSString *> * _Nullable)info NS_SWIFT_NAME(setCustomEvent(_:group:value:info:));
 + (void)setCustomEvent:(NSString *)name
                  group:(NSString * _Nullable)group
              snapshots:(NSString * _Nullable)snapshots
                  value:(double)value
-                  info:(NSDictionary<NSString *, NSString *> * _Nullable)info;
+                  info:(NSDictionary<NSString *, NSString *> * _Nullable)info NS_SWIFT_NAME(setCustomEvent(_:group:snapshots:value:info:));
 
-+ (void)setCustomLog:(NSString *)logInfo;
++ (void)setCustomLog:(NSString *)logInfo  NS_SWIFT_NAME(setCustomLog(_:));
 + (void)setCustomLog:(NSString *)logInfo
-                name:(NSString * _Nullable)name;
+                name:(NSString * _Nullable)name NS_SWIFT_NAME(setCustomLog(_:name:));
 + (void)setCustomLog:(NSString *)logInfo
                 name:(NSString * _Nullable)name
-           snapshots:(NSString * _Nullable)snapshots;
+           snapshots:(NSString * _Nullable)snapshots NS_SWIFT_NAME(setCustomLog(_:name:snapshots:));
 + (void)setCustomLog:(NSString *)logInfo
                 name:(NSString * _Nullable)name
            snapshots:(NSString * _Nullable)snapshots
                level:(NSString * _Nullable)level
-                info:(NSDictionary<NSString *, NSString *> * _Nullable)info;
+                info:(NSDictionary<NSString *, NSString *> * _Nullable)info NS_SWIFT_NAME(setCustomLog(_:name:snapshots:level:info:));
 
 + (BOOL)setCustomResource:(NSString *)type
                   success:(BOOL)success
                       url:(NSString *)url
                    method:(NSString *)method
                statusCode:(NSInteger)statusCode
-             errorMessage:(NSString * _Nullable)errorMessage
+             errorMessage:(NSString * _Nullable)errorMessage NS_SWIFT_NAME(setCustomResource(_:success:url:method:statusCode:errorMessage:))
     __attribute__((warn_unused_result));
 
 + (BOOL)setCustomResource:(NSString *)type
@@ -185,10 +183,10 @@ typedef NS_ENUM(NSInteger, AlibabaCloudTracingProtocol) {
              errorMessage:(NSString * _Nullable)errorMessage
                  provider:(NSString * _Nullable)provider
                   tracing:(AlibabaCloudTracingContext * _Nullable)tracing
-                  measure:(AlibabaCloudResourceMeasure * _Nullable)measure
+                  measure:(AlibabaCloudResourceMeasure * _Nullable)measure NS_SWIFT_NAME(setCustomResource(_:success:url:method:statusCode:errorMessage:provider:tracing:measure:))
     __attribute__((warn_unused_result));
 
-+ (void)setResourceProvider:(id<AlibabaCloudResourceProvider> _Nullable)provider;
++ (void)setResourceProvider:(id<AlibabaCloudResourceProvider> _Nullable)provider NS_SWIFT_NAME(setResourceProvider(_:));
 
 + (NSString *)getDeviceId __attribute__((warn_unused_result));
 
@@ -210,7 +208,7 @@ typedef NS_ENUM(NSInteger, AlibabaCloudTracingProtocol) {
 @property (nonatomic) NSUInteger downloadDuration;
 
 - (instancetype)init NS_DESIGNATED_INITIALIZER;
-+ (AlibabaCloudResourceMeasure *)measure __attribute__((warn_unused_result));
++ (AlibabaCloudResourceMeasure *)measure NS_SWIFT_NAME(measure()) __attribute__((warn_unused_result));
 
 @end
 
