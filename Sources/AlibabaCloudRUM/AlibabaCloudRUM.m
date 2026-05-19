4e49320903b95dae569f8c362676e65d17cb9859
yulong.gyl <yulong.gyl@alibaba-inc.com>
2026-05-19 13:16:36 +0800

    chore(release): release 2.2.8-beta.1

--- Sources/AlibabaCloudRUM/AlibabaCloudRUM.m
+++ Sources/AlibabaCloudRUM/AlibabaCloudRUM.m
@@ -21,7 +21,7 @@
 static NSString * _Nullable _endpoint  = nil;
 static NSString * _Nullable _workspace = nil;
 static NSString * _Nullable _env       = nil;
-static NSString *const RUM_SDK_VERSION = @"2.2.7";
+static NSString *const RUM_SDK_VERSION = @"2.2.8-beta.1";
 
 #pragma mark - Internal Class Extensions
 
