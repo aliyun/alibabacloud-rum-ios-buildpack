63e673e3fe72418c76dd92d0c01f51970b8aaaf2
yulong.gyl <yulong.gyl@alibaba-inc.com>
2026-06-24 11:30:07 +0800

    chore(ios): bump AlibabaCloudRUM to 2.2.10

--- Sources/AlibabaCloudRUM/AlibabaCloudRUM.m
+++ Sources/AlibabaCloudRUM/AlibabaCloudRUM.m
@@ -21,7 +21,7 @@
 static NSString * _Nullable _endpoint  = nil;
 static NSString * _Nullable _workspace = nil;
 static NSString * _Nullable _env       = nil;
-static NSString *const RUM_SDK_VERSION = @"2.2.9";
+static NSString *const RUM_SDK_VERSION = @"2.2.10";
 
 #pragma mark - Internal Class Extensions
 
