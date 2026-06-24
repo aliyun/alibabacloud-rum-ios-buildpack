3f23a34c27a8c6d6045767c10d5b7dc0efe8040e
yulong.gyl <yulong.gyl@alibaba-inc.com>
2026-06-24 21:42:22 +0800

    chore(ios): bump AlibabaCloudRUM to 2.2.11

--- AlibabaCloudRUM.podspec
+++ AlibabaCloudRUM.podspec
@@ -4,7 +4,7 @@
 
 Pod::Spec.new do |s|
     s.name             = 'AlibabaCloudRUM'
-    s.version          = "2.2.10"
+    s.version          = "2.2.11"
     s.summary          = 'AlibabaCloud RUM service for iOS.'
 
     s.description      = <<-DESC
--- Sources/AlibabaCloudRUM/AlibabaCloudRUM.m
+++ Sources/AlibabaCloudRUM/AlibabaCloudRUM.m
@@ -21,7 +21,7 @@
 static NSString * _Nullable _endpoint  = nil;
 static NSString * _Nullable _workspace = nil;
 static NSString * _Nullable _env       = nil;
-static NSString *const RUM_SDK_VERSION = @"2.2.10";
+static NSString *const RUM_SDK_VERSION = @"2.2.11";
 
 #pragma mark - Internal Class Extensions
 
