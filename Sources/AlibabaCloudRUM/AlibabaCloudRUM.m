4a895ff7825efad45a850096c9075262a9cb92f4
yulong.gyl <yulong.gyl@alibaba-inc.com>
2026-08-18 10:40:00 +0800

    chore(release): prepare 2.4.0 beta
    Update package metadata to 2.4.0-beta.1 and align the embedded SDK version with 2.4.0.

--- Sources/AlibabaCloudRUM/AlibabaCloudRUM.m
+++ Sources/AlibabaCloudRUM/AlibabaCloudRUM.m
@@ -21,7 +21,7 @@
 static NSString * _Nullable _endpoint  = nil;
 static NSString * _Nullable _workspace = nil;
 static NSString * _Nullable _env       = nil;
-static NSString *const RUM_SDK_VERSION = @"2.3.3";
+static NSString *const RUM_SDK_VERSION = @"2.4.0";
 
 #pragma mark - Internal Class Extensions
 
